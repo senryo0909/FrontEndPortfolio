763ee099e1234df6e987384c00680014f946571a
senryo0909 <19870909takuya@gmail.com>
2020-10-08 06:38:33 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// 'usestrict';
+// {
+//     const btn = document.getElementById('menuButton');
+//     btn.addEventListener('click', () => {
+//         this.classList.toggle('active');
+//     })
+// }
+
